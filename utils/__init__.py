c06d60d0d01489fe354af8c3c8ab747461d7b910
dosrui78 <dosrui78@gmail.com>
2022-08-07 20:21:44 +0800

    通用请求方式封装

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,10 +1,13 @@
 import requests
+import execjs
+import urllib3
 from retrying import retry
 from faker import Faker
 from loguru import logger
 from requests.exceptions import ConnectionError
 from requests import RequestException
 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 __all__ = ['Requests']
 
 
@@ -18,6 +21,17 @@ class Requests(object):
     def log(self, text):
         return logger.info(text)
 
+    def execJS(self, filepath, func_name, *args):
+        with open(filepath, 'r', encoding='utf8') as f:
+            js = f.read()
+        ctx = execjs.compile(js)
+        if isinstance(args, tuple):
+            if len(args) == 1:
+                return ctx.call(func_name, args[0])
+            if len(args) == 2:
+                return ctx.call(func_name, args[0], args[1])
+        return ctx.call(func_name)
+
     @retry(stop_max_attempt_number=3, wait_fixed=2000, retry_on_result=lambda x: x is None)
     def get_request(self, url, **kwargs):
         """
@@ -28,21 +42,17 @@ class Requests(object):
         kwargs.setdefault('verify', False)
         kwargs.setdefault('headers', self.headers)
         kwargs.setdefault('allow_redirects', False)
-        for retryCount in range(1, 6):
-            try:
-                if retryCount == 5:
-                    raise RequestException('第{}次重试失败， 已达最大重试次数'.format(retryCount))
-                res = self.session.get(url, **kwargs)
-                if res.status_code == 200:
-                    res.encoding = 'utf-8'
-                    self.log(f"第{retryCount}次请求成功，请求地址：{url}")
-                    return res
-                else:
-                    self.log(f"第{retryCount}次请求失败，请求地址：{url}")
-                    continue
-            except Exception as e:
-                # self.log.error(f"第{retryCount}次请求失败，请求地址：{url}")
-                raise e
+        try:
+            res = self.session.get(url, **kwargs)
+            if res.status_code == 200:
+                res.encoding = 'utf-8'
+                self.log(f"请求成功，请求地址：{url}")
+                return res
+            else:
+                self.log(f"请求地址：{url}， 状态码：{res.status_code}")
+        except Exception as e:
+            self.log(f"请求失败，请求地址：{url}")
+            raise e
 
     @retry(stop_max_attempt_number=3, wait_fixed=2000, retry_on_result=lambda x: x is None)
     def post_request(self, url, **kwargs):
@@ -54,18 +64,14 @@ class Requests(object):
         kwargs.setdefault('verify', False)
         kwargs.setdefault('headers', self.headers)
         kwargs.setdefault('allow_redirects', False)
-        for retryCount in range(1, 6):
-            try:
-                if retryCount == 5:
-                    raise RequestException('第{}次重试失败， 已达最大重试次数'.format(retryCount))
-                res = self.session.post(url, **kwargs)
-                if res.status_code == 200:
-                    res.encoding = 'utf-8'
-                    self.log(f"第{retryCount}次请求成功，请求地址：{url}")
-                    return res
-                else:
-                    self.log(f"第{retryCount}次请求失败，请求地址：{url}")
-                    continue
-            except Exception as e:
-                # self.log.error(f"第{retryCount}次请求失败，请求地址：{url}")
-                raise ConnectionError
+        try:
+            res = self.session.post(url, **kwargs)
+            if res.status_code == 200:
+                res.encoding = 'utf-8'
+                self.log(f"请求成功，请求地址：{url}")
+                return res
+            else:
+                self.log(f"请求地址：{url}， 状态码：{res.status_code}")
+        except Exception as e:
+            self.log(f"请求失败，请求地址：{url}")
+            raise e
